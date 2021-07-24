47264a10d274368eb8dd2aa3952293bd7491aa1f
yuchi0307 <gelee0307@gmail.com>
2021-07-24 16:22:10 +0800

    map svg render

---
+++ src/TaiwanMap.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// import map from './map.svg';
+import { ReactComponent as Map } from './map.svg';
+import './MapColor.css';
+
+
+const TaiwanMap =()=>{
+
+	return(
+		<Map className='App-map'/>
+	);
+}
+
+export default TaiwanMap;
