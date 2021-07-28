e762901ab82a31470137f4e60160cde2727f632c
SPP <hatsunemiku1144@gmail.com>
2021-07-28 20:34:51 +0800

    皇天不負苦心人

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
     super()
     this.state = {
       cities: cities,
-      ApiCity:[],
+      ApiCity: [],
       // Cityinfo: {
       //   tag: null,
       //   place: null,
@@ -21,12 +21,11 @@ class App extends Component {
     }
   }
 
-  componentDidMount()
-  {
+  componentDidMount() {
     const url = 'https://opendata.cwb.gov.tw/fileapi/v1/opendataapi/F-C0032-001?Authorization=CWB-3A5C9611-3227-4637-AA5B-554962990E24&format=JSON'
     fetch(url)
-    .then(resp => resp.json())
-    .then(data => this.setState({ApiCity: data.cwbopendata.dataset.location}))
+      .then(resp => resp.json())
+      .then(data => this.setState({ ApiCity: data.cwbopendata.dataset.location }))
 
   }
   // 這裡一定要用arrow function
@@ -34,23 +33,24 @@ class App extends Component {
   // 我們是要改變 App 的 this.state 
   //（也就是 this 是 App 的！若不寫 arrow function 的話他會以為this.state是子層的，但子層是沒有state的唷）
   onCityChange = (e) => {
-    const tagname = e.target.getAttribute('data-name')
-    console.log('亂碼：',tagname)
-    console.log('解碼：',tagname.normalize())
+    let tagname = e.target.getAttribute('data-name')
+    tagname = tagname.replaceAll('\\', '%');
+    console.log('亂碼：', tagname)
+    console.log('解碼：', unescape(tagname))
     //串假資料的部分
     /*const filteredCitys = this.state.cities.filter(function (city) {
       return city.tag === tagname //tagname為相應svg圖層的縣市
       //把svg圖層縣市的json值放入filter出來的陣列filteredCitys
     })*/
-    
-    const filteredCitys = this.state.ApiCity.filter(function (city){
+
+    const filteredCitys = this.state.ApiCity.filter(function (city) {
       return city.locationName === tagname.normalize('NFC')
     })
-    console.log('city.locationName',filteredCitys)
+    console.log('city.locationName', filteredCitys)
 
     let filteredCity = filteredCitys[0] //我的json資料在陣列[0]
     if (filteredCity === undefined) { //如果到非縣市的svg圖層
-      console.log('im null') 
+      console.log('im null')
       // filteredCity = { //給他null值才不會整個壞掉
       //   tag: null,
       //   place: null,
@@ -59,22 +59,22 @@ class App extends Component {
       //   weather: null
       // }
     }
-    console.log('過濾',filteredCity)
+    console.log('過濾', filteredCity)
     //假資料串接 console.log('選到的資料',filteredCity);
     //this.setState({ Cityinfo: filteredCity })
 
   }
   render() {
-    console.log('state',this.state.ApiCity);
+    console.log('state', this.state.ApiCity);
 
     return (
       <div>
         <div className='title' >
           <h1>TAIWAN<br />WEATHER APP</h1>
-            <hr />
-          
+          <hr />
+
           {/*假資料<CityInfo city={this.state.Cityinfo} />*/}
-           <CityInfo city={this.state.ApiCity} />
+          <CityInfo city={this.state.ApiCity} />
 
         </div>
         {/*當滑鼠移到該縣市時觸發filter
